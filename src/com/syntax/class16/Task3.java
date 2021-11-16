f616a93e4087e7093ee43aa9b18af5bdba22e9cc
Sorbon Imomnazarov <isorbon@gmail.com>
2021-11-16 17:15:41 +0200

    Added Class 16

--- src/com/syntax/class16/Task3.java
+++ src/com/syntax/class16/Task3.java
@@ -9,9 +9,11 @@ public class Task3 {
 	 */
 	
 	private static String processString(String str) {
+
 		return str.replaceAll("[^aeiouAEIOU]", "");
 	}
 	public static void main(String[] args) {
+
 		System.out.println(processString("Asghar"));
 	}
 	
